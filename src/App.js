f4542157ec984e783974bc86289f62b2f677c9b3
pendekar-koding <pendekarkoding20@gmail.com>
2021-02-14 14:54:48 +0700

    21

--- src/App.js
+++ src/App.js
@@ -12,28 +12,31 @@ import PrivateRoute from "./components/privateRoute";
 import FirebaseProvider from './components/firebaseProvider';
 
 //komponent material-ui
-import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline';
-import { CssBaseline } from '@material-ui/core';
+import {CssBaseline} from "@material-ui/core";
+import {ThemeProvider} from "@material-ui/core";
+import theme from "./components/config/theme";
 
 
 function App() {
   return (
     <>
-    <CssBaseline/>
-    <FirebaseProvider>
-    <Router>
-      <Switch>
-          <PrivateRoute path={"/"} exact component={Private}/>
-          <PrivateRoute path={"/pengaturan"} component={Private} />
-          <PrivateRoute path={"/produk"} component={Private} />
-          <PrivateRoute path={"/transaksi"} component={Private}/>
-          <Route path={"/registrasi"} component={Registrasi} />
-          <Route path={"/login"} component={Login} />
-          <Route path={"/lupa-password"} component={LupaPassword} />
-          <Route component={NotFound} />
-      </Switch>
-    </Router>
-    </FirebaseProvider>
+        <CssBaseline/>
+            <ThemeProvider theme={theme}>
+                <FirebaseProvider>
+                    <Router>
+                        <Switch>
+                            <PrivateRoute path={"/"} exact component={Private}/>
+                            <PrivateRoute path={"/pengaturan"} component={Private} />
+                            <PrivateRoute path={"/produk"} component={Private} />
+                            <PrivateRoute path={"/transaksi"} component={Private}/>
+                            <Route path={"/registrasi"} component={Registrasi} />
+                            <Route path={"/login"} component={Login} />
+                            <Route path={"/lupa-password"} component={LupaPassword} />
+                            <Route component={NotFound} />
+                        </Switch>
+                    </Router>
+                </FirebaseProvider>
+            </ThemeProvider>
     </>
   );
 }
--- src/pages/registrasi/index.js
+++ src/pages/registrasi/index.js
@@ -1,10 +1,15 @@
-import REact from "react";
-import styles from './index.module.css';
+import {Button} from "@material-ui/core";
+import useStyles from "./styles";
 
 
 function Registrasi(){
+    const classes = useStyles();
 
-    return <h1 className={styles.blue}>Halaman Registrasi</h1>
+    return<>
+        <h1 className={classes.blue}>Halaman Registrasi</h1>
+        <Button color={"primary"}
+        variant={"contained"}>Click</Button>
+    </>
 }
 
 export default Registrasi;
